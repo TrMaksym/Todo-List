cc5c02b4e87c5b2c66c36d45bd68df2416712166
unknown <maximuschampion2002@gmail.com>
2025-04-16 13:22:06 +0300

    Removed .env from tracking, added manage.py, initial migration, updated settings.py, requirements.txt, .gitignore

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)nqma8+2wbcu#*y-y+118wy(&^ri05ex4r*q-vs4ull6uie8p&'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
